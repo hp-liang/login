800f48a48d506fabc9e030b29b1e52d085c4dcff
hp-liang <1342850207@qq.com>
2019-06-18 12:07:20 +0800

    css-updata

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="ba">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on">
+      <h3>login</h3>
       <el-form-item>
-        <span>用户</span>
-        <el-input ref="user" v-model="loginForm.user" type="text" name="user" autocomplete="on" />
+        <span class="user">用户:</span>
+        <el-input ref="user" v-model="loginForm.user" class="user" type="text" name="user" autocomplete="on" />
       </el-form-item>
 
       <el-form-item>
-        <span>密码</span>
-        <el-input ref="password" v-model="loginForm.password" type="password" name="password" />
+        <span class="password">密码:</span>
+        <el-input ref="password" v-model="loginForm.password" class="password" type="password" name="password" />
       </el-form-item>
       <el-button class="bt" type="submit" @click="submitForm">提交</el-button>
+
+      <el-form-item>
+        <span class="item1">注册</span>
+        <span class="item2">忘记密码？</span>
+      </el-form-item>
     </el-form>
   </div>
 </template>
@@ -50,10 +56,9 @@ export default {
       const name = this.$refs.user.value
       const pass = this.$refs.password.value
       if (name === 'admin' && pass === '12345') {
-
-        // this.$router.push({
-        //   path: '/index'
-        // });
+        this.$router.push({
+          path: '/index'
+        })
       }
     }
 
@@ -65,46 +70,86 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 
-  $dark_gray:#889aa4;
+  $dark_gray:red;
 
   .ba{
     height: 100%;
     width: 100%;
-    left: 0;
+    left: 0px;
     bottom: 0;
-    background-image: url("../assets/lc.png") ;
+    background-image: url("../assets/gb.png") ;
     background-repeat: no-repeat;
     background-size: 100% 100% ;
     position: absolute;
 
     .login-form {
-      position: relative;
-      width: 520px;
+      position:relative;
+      width: 300px;
+      top: 50px;
+     text-align: center;
       max-width: 100%;
       padding: 160px 35px 0;
       margin: 0 auto;
       overflow: hidden;
+      border:red solid 1px;
     }
 
     span {
+      position: fixed;
       padding: 6px 6px;
       color: $dark_gray;
       vertical-align: middle;
       width: 60px;
       display: inline-block;
     }
+    span.user{
+      position: fixed;
+      top: 90px;
+      left: 620px;
+    }
+    span.password{
+      position: fixed;
+      top: 120px;
+      left: 620px;
+    }
+    span.item1{
+      position: fixed;
+      top: 190px;
+      left: 560px;
+      font-size:13px;
+    }
+    span.item2{
+      position: fixed;
+      top: 190px;
+      font-size:12px;
+      left: 620px;
+    }
+
+  }
+  .user{
+    position: fixed;
+    top: 93px;
+    left: 690px;
+  }
+  .password{
+    position: fixed;
+    top: 120px;
+    left: 690px;
+  }
+  h3{
+    position: fixed;
+    top: 40px;
+    left: 710px;
+    color: blueviolet;
   }
 
-  .lab{
-    font-size: 7px;
-    color: brown;
-    text-align: left;
-  }
   .bt{
-    padding: 2.5px 40px;
-  }
-  .padd{
-    padding: 3px;
+    padding: 2.5px 20px;
+    position: fixed;
+    background-color: antiquewhite;
+    color: blue;
+    top: 150px;
+    left: 720px;
   }
 
 </style>
